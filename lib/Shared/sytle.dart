ec12d635e7a772a57df2a1739dbbae21e00e93c6
EVILLE VITÓRIA NUNES COELHO <eville.coelho@a.catolica-to.edu.br>
2025-08-14 20:18:07 -0300

    Primeiro commit

---
+++ lib/Shared/sytle.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle regular = TextStyle (
+  fontFamily: "Inter",
+  fontSize:  40,
+  fontWeight: FontWeight.w300,
+  color: Color(0x00000000),
+  letterSpacing: 1.5
+);
+
+const TextStyle bold = TextStyle (
+    fontFamily: "Inter",
+    fontSize:  40,
+    fontWeight: FontWeight.bold,
+    color: Color(0x00000000),
+    letterSpacing: 1.5
+);
+
+const TextStyle italic = TextStyle (
+    fontFamily: "Inter",
+    fontSize:  40,
+    fontWeight: FontWeight.w300,
+    fontStyle: FontStyle.italic,
+    color: Color(0x00000000),
+    letterSpacing: 1.5
+);
