731aa4d82e7d15306b9f0282a3a4f31ca2d0cbea
Eville V. N. Coelho <evillevnc@tce-to.tce.to.gov.br>
2025-11-06 14:41:07 -0300

    correções - VM

---
+++ lib/design_system/viewmodels/sentiment_bubble_viewmodel.dart
@@ -0,0 +1,12 @@
+// lib/design_system/widgets/viewmodels/sentiment_bubble_viewmodel.dart
+import 'package:flutter/material.dart';
+
+class SentimentBubbleViewModel {
+  final String sentiment;
+  final Color color;
+
+  SentimentBubbleViewModel({
+    required this.sentiment,
+    required this.color,
+  });
+}
